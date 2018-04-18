0c3172fa7758234b07618b998796761f2b5c3a62
YuexiWangwei <wweihappy@126.com>
2018-04-18 13:45:26 +0800

    修改了图片保存后变小，以及可以定义图片开始序号

--- 蚕茧图片框选/imgframe.py
+++ 蚕茧图片框选/imgframe.py
@@ -6,9 +6,10 @@ from tkinter import filedialog
 from tkinter import colorchooser
 from tkinter.scrolledtext import ScrolledText
 from PIL import Image
+import shutil
 # import matplotlib.pyplot as plt # plt 用于显示图片,调试使用！
 import numpy as np
-import scipy.misc
+#import scipy.misc
 import xlwt
 
 
@@ -75,6 +76,14 @@ def choosecolor():      #得到用户需要用的颜色R,G,B
     return colorrgb
 
 def goin():
+
+    root.destroy()
+    secondwindow = Tk()
+    secondwindow.title("自动框选蚕茧")
+    labelinput = Label(secondwindow, text="输入开始序号：", font="幼圆")
+    intnumber = IntVar
+    numberentry = Entry(secondwindow, width=18, textvariable=intnumber)
+
     def start():
         r = colorrgb[0]
         g = colorrgb[1]
@@ -93,6 +102,10 @@ def goin():
         sheet1 = relatedexcel.add_sheet('Sheet1')
         sheet1.write(0, 0, "图片原名称")
         sheet1.write(0, 1, "修改后名称")
+
+        #定义文件的序号
+        number = int(numberentry.get())
+        print(number)
 
         for each in range(n):
             fileimg = path + filelists[each]  # 得到图片的完整路径
@@ -154,26 +167,26 @@ def goin():
                         if i == 5:
                             y1 = y - yuzhi
                             break
-
-            # 以下代码将图片进行框选
-            for x in range(Y):
-                for y in range(X):
-                    if x >= x1 - width and x <= x1 + width and y >= y1 and y <= y2:
-                        imgarr[x, y, 0] = r
-                        imgarr[x, y, 1] = g
-                        imgarr[x, y, 2] = b
-                    elif x >= x2 - width and x <= x2 + width and y >= y1 and y <= y2:
-                        imgarr[x, y, 0] = r
-                        imgarr[x, y, 1] = g
-                        imgarr[x, y, 2] = b
-                    elif y >= y1 - width and y <= y1 + width and x >= x1 and x <= x2:
-                        imgarr[x, y, 0] = r
-                        imgarr[x, y, 1] = g
-                        imgarr[x, y, 2] = b
-                    elif y >= y2 - width and y <= y2 + width and x >= x1 and x <= x2:
-                        imgarr[x, y, 0] = r
-                        imgarr[x, y, 1] = g
-                        imgarr[x, y, 2] = b
+            #
+            # # 以下代码将图片进行框选
+            # for x in range(Y):
+            #     for y in range(X):
+            #         if x >= x1 - width and x <= x1 + width and y >= y1 and y <= y2:
+            #             imgarr[x, y, 0] = r
+            #             imgarr[x, y, 1] = g
+            #             imgarr[x, y, 2] = b
+            #         elif x >= x2 - width and x <= x2 + width and y >= y1 and y <= y2:
+            #             imgarr[x, y, 0] = r
+            #             imgarr[x, y, 1] = g
+            #             imgarr[x, y, 2] = b
+            #         elif y >= y1 - width and y <= y1 + width and x >= x1 and x <= x2:
+            #             imgarr[x, y, 0] = r
+            #             imgarr[x, y, 1] = g
+            #             imgarr[x, y, 2] = b
+            #         elif y >= y2 - width and y <= y2 + width and x >= x1 and x <= x2:
+            #             imgarr[x, y, 0] = r
+            #             imgarr[x, y, 1] = g
+            #             imgarr[x, y, 2] = b
 
             #找到蚕茧的种类,生成标签时插入
             kinds = []
@@ -193,17 +206,21 @@ def goin():
             # xmlname = filename[0] + ".xml"
 
             #用数字的方式保存的图片名称与xml文件名称
-            tempname = "%06d"%(each + 1)
+            tempname = "%06d"%(number)
+            number += 1
             xmlname =tempname + ".xml"
-            filename =tempname + ".png"
+            filename =tempname + ".jpg"
 
             try:
                 # 以新名字保存图片
                 # plt.imshow(imgarr)
                 # plt.axis('off')
                 # plt.savefig(filename)
+                os.chdir(path)
+                shutil.copy(filelists[each],path+"jpg")
                 os.chdir(path + "jpg")
-                scipy.misc.imsave(filename,imgarr)
+                os.rename(filelists[each],filename)
+                #scipy.misc.imsave(filename,img)
 
                 # 以新图片名字保存相应xml文件
                 os.chdir(path + "xml")
@@ -234,17 +251,15 @@ def goin():
         os.chdir(path)
         relatedexcel.save("relatedexcel.xls")
 
-    root.destroy()
-    secondwindow = Tk()
-    secondwindow.title("自动框选蚕茧")
-
     textshow = ScrolledText(secondwindow, width=50, height=20, font="幼圆")
     startbutton = Button(secondwindow, text="开始框选", command=start, font="幼圆")
     closebutton = Button(secondwindow, text=" 退   出 ", command=close, font="幼圆")
 
     textshow.grid(rowspan=5, columnspan=2, padx=5, pady=10)
-    startbutton.grid(row=2, column=2, padx=25, pady=15)
-    closebutton.grid(row=3, column=2, padx=25, pady=15)
+    labelinput.grid(row = 1,column = 2,padx = 25,pady = 5)
+    numberentry.grid(row=2, column=2, padx=25, pady=1)
+    startbutton.grid(row=3, column=2, padx=25, pady=15)
+    closebutton.grid(row=4, column=2, padx=25, pady=15)
 
     mainloop()
 
